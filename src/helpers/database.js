f8081b94b5cb75f832a2304084955505dc88d7ae
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 22:33:20 +0700

    fix : remove env mysql port

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
 
-const { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLDATABASE,MYSQLPORT } = process.env;
+const { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLDATABASE } = process.env;
 const sequelize = new Sequelize(MYSQLDATABASE, MYSQLUSER, MYSQLPASSWORD, {
   host: MYSQLHOST,
-  port:MYSQLPORT,
   dialect: 'mysql'
 });
 
