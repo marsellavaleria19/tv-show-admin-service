ce0b2192bd56d2a2575f4de371c545825dc4ebd7
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 16:08:06 +0700

    fix : add my sql port

--- src/helpers/database.js
+++ src/helpers/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLDATABASE } = process.env;
+const { MYSQLHOST, MYSQLUSER, MYSQLPASSWORD, MYSQLDATABASE,MYSQLPORT } = process.env;
 const sequelize = new Sequelize(MYSQLDATABASE, MYSQLUSER, MYSQLPASSWORD, {
   host: MYSQLHOST,
+  port:MYSQLPORT,
   dialect: 'mysql'
 });
 
