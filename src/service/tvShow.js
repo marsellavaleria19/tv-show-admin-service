f531d6a366124558c522fa61e5e080a75e3b6c1f
marsellavaleria19 <marsellavaleria19@gmail.com>
2025-12-25 22:31:54 +0700

    refactor : change change url api public using env

--- src/service/tvShow.js
+++ src/service/tvShow.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 exports.fetchShowFromApi = async () => {
   try {
-    const response = await axios.get('https://api.tvmaze.com/shows')
+    const response = await axios.get(`${process.env.PUBLIC_API_URL}`)
     return response.data
   } catch (error) {
     throw error
